02324078ac7109756f473031338e040983bccf54
Frederic Fox <dev.frederic.fox@gmail.com>
2022-02-23 17:32:42 +0100

    Change wording

--- server/api/publicEndpoint.ts
+++ server/api/publicEndpoint.ts
@@ -3,7 +3,7 @@ import * as functions from 'firebase-functions';
 
 export default async (req, res) => {
   const cookies = useCookies(req)
-  functions.logger.log("cookies:",cookies);
+  functions.logger.log("public endpoint cookies:",cookies);
   const message = "I am the public route"
   return { message, cookies }
 }
