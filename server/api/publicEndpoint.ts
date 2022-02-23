5dc2d256990cf7594c9e40e95e7dcb8328690966
Frederic Fox <dev.frederic.fox@gmail.com>
2022-02-23 15:05:40 +0100

    Add Firebase loging

--- server/api/publicEndpoint.ts
+++ server/api/publicEndpoint.ts
@@ -1,7 +1,9 @@
 import { useCookies, useQuery } from 'h3'
+import * as functions from 'firebase-functions';
 
 export default async (req, res) => {
   const cookies = useCookies(req)
+  functions.logger.log("cookies:",cookies);
   const message = "I am the public route"
   return { message, cookies }
 }
--- server/middleware/admin.ts
+++ server/middleware/admin.ts
@@ -1,12 +1,17 @@
+import * as functions from 'firebase-functions';
 import type { IncomingMessage, ServerResponse } from 'http'
 import { useCookie } from 'h3';
 
 export default async (req: IncomingMessage, res: ServerResponse,next) => {
     if (req.url.includes('/api/privateEndpoint')) {
-    console.log(req.headers.cookie)
     const token = useCookie(req, 'test');
-    console.log(token)
     const message = `I am the middleware. My received cookie is: ${token}`
+    
+    console.log(req.headers.cookie)
+    console.log(token)
+    functions.logger.log('header cookie',req.headers.cookie);
+    functions.logger.log('Token:',token);
+
     res.end(message)
     }
     next()
